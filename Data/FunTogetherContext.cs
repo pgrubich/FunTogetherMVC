17506d5f98bea598990d4ae072d996ee3d37b451
pgrubich <pamelagrubich@gmail.com>
2020-12-11 09:52:20 +0100

    Initial commit

---
+++ Data/FunTogetherContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FunTogether.Models;
+
+namespace FunTogether.Data
+{
+    public class FunTogetherContext : DbContext
+    {
+        public FunTogetherContext(DbContextOptions<FunTogetherContext> options) : base(options)
+        {
+        }
+
+    }
+}
