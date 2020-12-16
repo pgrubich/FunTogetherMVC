f65edca737f48c8b32c6931e59052e8dcfa71324
pgrubich <pamelagrubich@gmail.com>
2020-12-16 10:55:30 +0100

    Add ActivityTitle Migration

--- Migrations/20201216094157_AddActivityTitle.Designer.cs
+++ Migrations/20201216094157_AddActivityTitle.Designer.cs
@@ -4,14 +4,16 @@ using FunTogether.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FunTogether.Migrations
 {
     [DbContext(typeof(FunTogetherContext))]
-    partial class FunTogetherContextModelSnapshot : ModelSnapshot
+    [Migration("20201216094157_AddActivityTitle")]
+    partial class AddActivityTitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
